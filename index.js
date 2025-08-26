22a39e569be7fb0220e4b6d36a71b8edd2ffa942
Pratush <pratushbarua26@gmail.com>
2025-08-27 00:05:39 +0600

    complete navbar functionality

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
+// some utility veriables
 let heartBtns = document.querySelectorAll("#container span div:first-of-type button");
-let callBtns = document.querySelectorAll("#container span div:last-of-type button:last-of-type");
 let copyBtns = document.querySelectorAll("#container span div:last-of-type button:first-of-type");
+let callBtns = document.querySelectorAll("#container span div:last-of-type button:last-of-type");
 let historyContainer = document.querySelector("aside>section");
-let ClearHistory = function() {
+
+// some utility functions
+let ClearHistory = function () {
     historyContainer.innerHTML = "";
 }
+
+// event listenrs
 heartBtns.forEach(btn => {
     btn.addEventListener("click", (e) => {
-        alert("adding...");
-});
-});
-callBtns.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        alert("Calling...");
-});
+        let heartCouter = document.getElementById("heart");
+        heartCouter.innerText = parseInt(heartCouter.innerText) + 1;
+        alert("Heart added");
+    });
 });
 copyBtns.forEach(btn => {
     btn.addEventListener("click", (e) => {
-        alert("Copying...");
+        let text = e.target.parentElement.parentElement.querySelector("h3").innerText;
+        alert(`Copied: ${text}`);
+        let copyCouter = document.querySelector("nav button>span");
+        copyCouter.innerText = parseInt(copyCouter.innerText) + 1;
+        navigator.clipboard.writeText(text)
+    });
 });
+callBtns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        let text = e.target.parentElement.parentElement.querySelector("h3").innerText;
+        let coin = document.getElementById("coin");
+        if (parseInt(coin.innerText) >= 20) {
+            alert(`Calling - ${text}`);
+            coin.innerText = parseInt(coin.innerText) - 20;
+        }else alert("You don't have enough coins");
+    });
 });
